53837151d365cd1b15fb6dd1f7b180e6cead7f46
Harun Doğdu <harundogdu06@gmail.com>
2022-01-02 22:25:04 +0300

    favourites component designed

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import Title from "components/ui/Title";
+import favouritesData from "api/favourites.json";
+import {AiOutlinePlus} from "react-icons/ai"
 
 const Favourites = () => {
-  return (
-    <>
-      Favourites
-    </>
-  )
-}
+  const [favourites, setFavourites] = React.useState([]);
 
-export default Favourites
+  React.useEffect(() => {
+    setFavourites(favouritesData);
+  }, []);
+
+  return (
+    <div className="bg-mainBg">
+      <div className="mx-auto container px-28 py-5">
+        <Title>Favoriler</Title>
+        <div className="bg-white flex p-3 grid-cols-4 divide-x divide-y divide-gray-50 flex-wrap items-center">
+          {
+            favourites.map((favourite, index) => (
+              <div key={favourite.id} className="w-44 h-52 flex flex-col flex-grow items-center justify-center p-2 cursor-pointer relative">
+                <img className="w-28 h-28 object-cover" src={favourite.image} alt={favourite.alt} />
+                <h6 className="text-activeColor font-semibold">{favourite.price}₺</h6>
+                <h6 className="font-semibold text-sm text-center">{favourite.title}</h6>
+                <h6 className="text-sm text-gray-400">{favourite.alt}</h6>
+                <div className="absolute top-1 right-4 bg-white shadow-md p-2 rounded-lg"><AiOutlinePlus className="text-activeColor"  size={16}/></div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Favourites;
